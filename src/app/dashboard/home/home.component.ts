541dc1457bc89e580e1f1f48cc12411c5ab40caa
sys2612 <nageswar@way2online.net>
2019-04-04 16:04:54 +0530

    s

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import cities from '../../../assets/json/cities.json';
+
 
 @Component({
   selector: 'app-home',
@@ -7,15 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  myCity:any;
   clicked = true;
-  constructor() {
+  
+  constructor(
+    private router: Router
+  ) {
     
    }
 
   ngOnInit() {
+    this.myCity=cities;
   }
+
+
   clickFunc(){
     this.clicked = !this.clicked;
     console.log(this.clicked);
+  }
+  onClick(){
+    this.router.navigate(['send']);
   }
 }
